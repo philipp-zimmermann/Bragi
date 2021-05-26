56387dd9cdc636e5829ab5ef91c65e0570ebf133
Philipp Zimmermann <zimmermp@cs.uni-kl.de>
2021-05-26 15:09:10 +0200

    [InitMacros] add header for variadic macros
    Signed-off-by: Philipp Zimmermann <zimmermp@cs.uni-kl.de>

---
+++ src/LoggingUtilityMacros.h
@@ -0,0 +1,35 @@
+#ifndef _ML_UTILITY_MACROS_H_
+#define _ML_UTILITY_MACROS_H_
+
+#define _ML_INIT_LOCAL(sourceClass, localConfig)                        \
+  template <marsLogging::LogLevel messageLevel>                         \
+  constexpr static inline auto log_message() noexcept                   \
+  {                                                                     \
+    return marsLogging::Log<messageLevel, sourceClass,                  \
+                            static_cast<int16_t>(localConfig.logLevel), \
+                            localConfig.enable>{};                      \
+  }
+
+#define _ML_INIT_GLOBAL(sourceClass)                      \
+  template <marsLogging::LogLevel messageLevel>           \
+  constexpr static inline auto log_message() noexcept     \
+  {                                                       \
+    return marsLogging::Log<messageLevel, sourceClass>{}; \
+  }
+
+#define _ML_INIT_DEFAULT()                            \
+  template <marsLogging::LogLevel messageLevel>       \
+  constexpr static inline auto log_message() noexcept \
+  {                                                   \
+    return marsLogging::Log<messageLevel>{};          \
+  }
+
+#define _ML_FUNC_CHOOSER(_f1, _f2, _f3, ...) _f3
+#define _ML_FUNC_RECOMPOSER(argsWithParentheses) _ML_FUNC_CHOOSER argsWithParentheses
+#define _ML_CHOOSE_FROM_ARG_COUNT(...) \
+  _ML_FUNC_RECOMPOSER((__VA_ARGS__, _ML_INIT_LOCAL, _ML_INIT_GLOBAL, ))
+#define _ML_NO_ARG_EXPANDER() , , _ML_INIT_DEFAULT
+#define _ML_MACRO_CHOOSER(...) \
+  _ML_CHOOSE_FROM_ARG_COUNT(_ML_NO_ARG_EXPANDER __VA_ARGS__())
+
+#endif
